d9c4ace52262ef9dd5b049f3e0259284268878af
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-16 16:28:12 -0300

    add Service controller

---
+++ Assets/@Game/Scripts/Controller/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+namespace Game.Scripts.Controller
+{
+    public static class Service<T> where T : class, new()
+    {
+        static T _instance;
+
+        public static T Get()
+        {
+            if (null == _instance)
+            {
+                _instance = new T();
+                Disposer.OnDispose += Dispose;
+            }
+
+            return _instance;
+        }
+
+        static void Dispose()
+        {
+            _instance = null;
+            Disposer.OnDispose -= Dispose;
+        }
+    }
+
+    class Disposer : MonoBehaviour
+    {
+        public static event Action OnDispose;
+
+        void OnDestroy()
+        {
+            OnDispose?.Invoke();
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Init()
+        {
+            GameObject go = new("[DISPOSER]");
+            DontDestroyOnLoad(go);
+            go.AddComponent<Disposer>();
+        }
+    }
+
+}
