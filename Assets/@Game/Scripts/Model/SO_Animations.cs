6737e80880c211fa4f95dc943c4120006c23671a
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-16 16:03:43 -0300

    add animations and store GDs

---
+++ Assets/@Game/Scripts/Model/SO_Animations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.AssetImporters;
+#endif
+
+namespace Game.Scripts.Model
+{
+    public class SO_Animations : ScriptableObject
+    {
+        public Animations[] animations;
+    }
+
+    [Serializable]
+    public class Animations
+    {
+        public string animationControllerName;
+        public string setItemName;
+        public string weaponItemName;
+    }
+
+#if UNITY_EDITOR
+    [ScriptedImporter(2, "animationsCSV")]
+    public class AnimationsImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            string text = File.ReadAllText(ctx.assetPath);
+            string[] lines = text.Split('\n');
+
+            List<Animations> animationsList = new();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] data = line.Split(',');
+                Animations row = new()
+                {
+                    animationControllerName = data[0],
+                    setItemName = data[1],
+                    weaponItemName = data[2]
+                };
+                animationsList.Add(row);
+            }
+
+            SO_Animations scriptableObject = ScriptableObject.CreateInstance<SO_Animations>();
+            scriptableObject.animations = animationsList.ToArray();
+            ctx.AddObjectToAsset("mainasset", scriptableObject);
+            ctx.SetMainObject(scriptableObject);
+        }
+    }
+#endif
+}
---
+++ Assets/@Game/Scripts/Model/SO_Store.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.AssetImporters;
+#endif
+namespace Game.Scripts.Model
+{
+    public class SO_Store : ScriptableObject
+    {
+        public Store[] storeItems;
+    }
+
+    [Serializable]
+    public class Store
+    {
+        public string itemName;
+        public int price;
+        public ItemType itemType;
+    }
+
+    public enum ItemType
+    {
+        SET,
+        WEAPON
+    }
+
+#if UNITY_EDITOR
+    [ScriptedImporter(2, "storeCSV")]
+    public class StoreImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            string text = File.ReadAllText(ctx.assetPath);
+            string[] lines = text.Split('\n');
+
+            List<Store> storeList = new();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] data = line.Split(',');
+                Store row = new()
+                {
+                    itemName = data[0],
+                    price = int.Parse(data[1], CultureInfo.InvariantCulture),
+                    itemType = Enum.Parse<ItemType>(data[2], true)
+                };
+                storeList.Add(row);
+            }
+
+            SO_Store scriptableObject = ScriptableObject.CreateInstance<SO_Store>();
+            scriptableObject.storeItems = storeList.ToArray();
+            ctx.AddObjectToAsset("mainasset", scriptableObject);
+            ctx.SetMainObject(scriptableObject);
+        }
+    }
+#endif
+}
