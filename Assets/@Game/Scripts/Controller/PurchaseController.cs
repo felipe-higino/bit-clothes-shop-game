b17b7828b15e10e4561bf6804860aec6a66d1a23
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-17 13:08:50 -0300

    add purchase controller draft

---
+++ Assets/@Game/Scripts/Controller/PurchaseController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+namespace Game.Scripts.Controller
+{
+    public class PurchaseController
+    {
+        public void Purchase()
+        {
+            Debug.Log("something purchased!");
+        }
+    }
+}
--- Assets/@Game/Scripts/Controller/ShopController.cs
+++ Assets/@Game/Scripts/Controller/ShopController.cs
@@ -2,6 +2,7 @@
 using Game.Scripts.View;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 namespace Game.Scripts.Controller
@@ -12,6 +13,8 @@ namespace Game.Scripts.Controller
         [SerializeField] GroupSelector _storeItemsContainer;
         [SerializeField] PurchaseWidget _purchaseWidgetPrefab;
         [SerializeField] Image _img_itemPreview;
+        [SerializeField] TMP_Text _txt_price;
+        [SerializeField] Button _btn_purchase;
 
         readonly Dictionary<GroupSelector.ISelectable, Store> _selectableStores = new();
         readonly Dictionary<Store, Sprite> _storeSprites = new();
@@ -39,18 +42,28 @@ namespace Game.Scripts.Controller
             _storeItemsContainer.OnChangeSelected += OnChangeSelectedItem;
             _storeItemsContainer.Init();
             _selectableStores.ElementAt(0).Key.SelectThis();
+
+            _btn_purchase.onClick.AddListener(OnClickPurchase);
         }
 
         void OnDestroy()
         {
             _storeItemsContainer.OnChangeSelected -= OnChangeSelectedItem;
+            _btn_purchase.onClick.RemoveListener(OnClickPurchase);
         }
 
         void OnChangeSelectedItem(GroupSelector.ISelectable selected)
         {
             Store storeItem = _selectableStores[selected];
             Sprite sprite = _storeSprites[storeItem];
+
             _img_itemPreview.sprite = sprite;
+            _txt_price.text = storeItem.price.ToString();
+        }
+
+        void OnClickPurchase()
+        {
+            Service<PurchaseController>.Get().Purchase();
         }
     }
 }
