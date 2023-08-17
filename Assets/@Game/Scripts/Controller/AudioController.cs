3448a566c53c28a3548b3a9f1cf5737fd19c4b92
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-16 23:01:56 -0300

    add audio clips and manager

---
+++ Assets/@Game/Scripts/Controller/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+namespace Game.Scripts.Controller
+{
+    public class AudioController : MonoBehaviour
+    {
+        [SerializeField] AudioSource _sfx;
+        public static AudioController Instance { get; private set; }
+
+        void Awake()
+        {
+            if (null != Instance)
+            {
+                Debug.LogError("Only one audio controller singleton allowed");
+                Destroy(this);
+            }
+
+            Instance = this;
+        }
+
+        public void PlaySFX(AudioClip clip)
+        {
+            _sfx.PlayOneShot(clip);
+        }
+    }
+}
---
+++ Assets/@Game/Scripts/Controller/AudioPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+namespace Game.Scripts.Controller
+{
+    public class AudioPlayer : MonoBehaviour
+    {
+        [SerializeField] AudioClip _audioClip;
+
+        public void Play()
+        {
+            AudioController.Instance.PlaySFX(_audioClip);
+        }
+    }
+}
