834e1b172643241dc0cd6574ddb3ad2d86397d9f
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-18 03:48:24 -0300

    add initial fade

--- Assets/@Game/Scripts/Controller/CoreloopController.cs
+++ Assets/@Game/Scripts/Controller/CoreloopController.cs
@@ -1,4 +1,6 @@
 ﻿using Game.Scripts.Model;
+using Game.Scripts.View;
+using System.Collections;
 using UniRx;
 using UnityEngine;
 namespace Game.Scripts.Controller
@@ -6,19 +8,30 @@ namespace Game.Scripts.Controller
     public class CoreloopController : MonoBehaviour
     {
         [SerializeField] GameObject _shopUIObject;
+        [SerializeField] FadeView _initialFade;
 
         void Awake()
         {
+            _initialFade.SetAlpha(1);
+
             Service<DatabusInventory>.Get().Cash = 2000;
 
             DatabusCoreloop coreloopDatabus = Service<DatabusCoreloop>.Get();
-
-            coreloopDatabus.gameState.Value = DatabusCoreloop.GameState.EXPLORING;
 
             coreloopDatabus.gameState
                 .ObserveOnMainThread()
                 .Subscribe(OnGamestateUpdate)
                 .AddTo(this);
+        }
+
+        IEnumerator Start()
+        {
+            yield return null;
+            yield return null;
+
+            _initialFade.FadeIn();
+
+            Service<DatabusCoreloop>.Get().gameState.Value = DatabusCoreloop.GameState.EXPLORING;
         }
 
         void OnGamestateUpdate(DatabusCoreloop.GameState gameState)
---
+++ Assets/@Game/Scripts/View/FadeView.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Game.Scripts.View
+{
+    public class FadeView : MonoBehaviour
+    {
+        [SerializeField] float _duration;
+        [SerializeField] Image _fade;
+
+        public void SetAlpha(float alpha)
+        {
+            Color color = _fade.color;
+            color.a = 1;
+            _fade.color = color;
+        }
+
+        public void FadeIn()
+        {
+            SetAlpha(1);
+            _fade
+                .DOFade(0, _duration)
+                .OnComplete(() => gameObject.SetActive(false));
+        }
+    }
+}
