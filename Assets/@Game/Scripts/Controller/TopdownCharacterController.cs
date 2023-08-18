919d032a94d03a66a174823302b561bb1130d48e
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-17 22:58:42 -0300

    add character controller

---
+++ Assets/@Game/Scripts/Controller/TopdownCharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+namespace Game.Scripts.Controller
+{
+    public class TopdownCharacterController : MonoBehaviour, BitClothesInputActions.IGameplayActions
+    {
+        [SerializeField] float _speed;
+        [SerializeField] Rigidbody2D _rigidbody2D;
+
+        Vector2 _direction;
+
+        //TODO: change to Kinematic Character Controller (KCC)
+        void Awake()
+        {
+            BitClothesInputActions actions = new();
+            actions.Gameplay.SetCallbacks(this);
+            actions.Enable();
+        }
+
+        void FixedUpdate()
+        {
+            _rigidbody2D.AddForce(_speed * Time.fixedDeltaTime * _direction);
+        }
+
+        void BitClothesInputActions.IGameplayActions.OnMove(InputAction.CallbackContext context)
+        {
+            _direction = context.ReadValue<Vector2>();
+        }
+    }
+}
