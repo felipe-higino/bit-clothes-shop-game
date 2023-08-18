18b8c0e576ff1964627ddfc7f62310a7f586f218
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-18 01:42:22 -0300

    add interaction colliders

---
+++ Assets/@Game/Scripts/Controller/InteractionShopController.cs
@@ -0,0 +1,14 @@
+﻿using Game.Scripts.View;
+using UnityEngine;
+namespace Game.Scripts.Controller
+{
+    public class InteractionShopController : MonoBehaviour, IInteractableCollider
+    {
+        [SerializeField] GameObject _interactionFeedback;
+
+        public void SetIsInteractable(bool isInteractable)
+        {
+            _interactionFeedback.SetActive(isInteractable);
+        }
+    }
+}
---
+++ Assets/@Game/Scripts/View/InteractorCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+namespace Game.Scripts.View
+{
+    public class InteractorCollider : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            IInteractableCollider interactableCollider = other.GetComponent<IInteractableCollider>();
+            if (null != interactableCollider)
+            {
+                interactableCollider.SetIsInteractable(true);
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            IInteractableCollider interactableCollider = other.GetComponent<IInteractableCollider>();
+            if (null != interactableCollider)
+            {
+                interactableCollider.SetIsInteractable(false);
+            }
+        }
+    }
+
+    interface IInteractableCollider
+    {
+        public void SetIsInteractable(bool isInteractable);
+    }
+}
