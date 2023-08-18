2141f51583f437fd76b35f0a982349620c41fae9
Felipe Higino <felipe.higino.ps@gmail.com>
2023-08-18 03:23:23 -0300

    add npc interactions

---
+++ Assets/@Game/Scripts/Controller/InteractionNpcController.cs
@@ -0,0 +1,57 @@
+﻿using Game.Scripts.View;
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace Game.Scripts.Controller
+{
+    public class InteractionNpcController : MonoBehaviour, IInteractableCollider
+    {
+        [SerializeField] NpcInteractionWidget _npcInteractionWidget;
+
+        bool _isInteractable;
+        IDisposable _timer;
+
+        void Awake()
+        {
+            Service<InputController>.Get().actions.Gameplay.Interact.performed += OnInteract;
+            _npcInteractionWidget.StateInvisible();
+        }
+
+        void OnDestroy()
+        {
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+        void OnInteract(InputAction.CallbackContext obj)
+        {
+            if (!_isInteractable)
+                return;
+
+            _npcInteractionWidget.StateResponse();
+            _timer?.Dispose();
+            _timer = null;
+            _timer = Observable
+                .Timer(TimeSpan.FromSeconds(3))
+                .ObserveOnMainThread()
+                .Subscribe(x => { _npcInteractionWidget.StateInteraction(); });
+        }
+
+        void IInteractableCollider.SetIsInteractable(bool isInteractable)
+        {
+            _isInteractable = isInteractable;
+
+            if (isInteractable)
+            {
+                _npcInteractionWidget.StateInteraction();
+            }
+            else
+            {
+                _npcInteractionWidget.StateInvisible();
+                _timer?.Dispose();
+                _timer = null;
+            }
+        }
+    }
+}
---
+++ Assets/@Game/Scripts/View/NpcInteractionWidget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+namespace Game.Scripts.View
+{
+    public class NpcInteractionWidget : MonoBehaviour
+    {
+        [SerializeField] Canvas _interactionView;
+        [SerializeField] Canvas _responseView;
+
+        public void StateInvisible()
+        {
+            _interactionView.enabled = false;
+            _responseView.enabled = false;
+        }
+
+        public void StateInteraction()
+        {
+            _interactionView.enabled = true;
+            _responseView.enabled = false;
+        }
+
+        public void StateResponse()
+        {
+            _interactionView.enabled = false;
+            _responseView.enabled = true;
+        }
+    }
+}
